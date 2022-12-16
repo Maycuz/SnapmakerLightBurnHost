e7443c594f1ec1dc26ed5b8b693f98de34ccac5e
Mayco <tristan@polymerprints.nl>
2022-12-16 20:26:18 +0100

    connection etc

--- SnapmakerLightburnHost/SnapmakerLightburnHost.cpp
+++ SnapmakerLightburnHost/SnapmakerLightburnHost.cpp
@@ -7,6 +7,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <format>
+#include <fstream>
+#include <filesystem>
+#include <time.h>
 
 #include "softcam.h"
 #include "stb_image.h"
@@ -15,43 +19,112 @@
 
 using namespace std;
 
-//const string ipAddress = "192.168.178.89";
-//
-//void getImageFromSnapmaker(CURL *curlInstance, string ipAddress)
-//{
-//
-//}
+const string ipAddress = "192.168.178.89";
+std::filesystem::path targetFile;
+
+string GetTimeStamp()
+{
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&in_time_t), "[%d-%m %X] ");
+    return ss.str();
+}
+
+// Source: https://cplusplus.com/forum/general/46477/
+// callback function writes data to a std::ostream
+static size_t data_write(void* buf, size_t size, size_t nmemb, void* userp)
+{
+    if (userp)
+    {
+        std::ostream& os = *static_cast<std::ostream*>(userp);
+        std::streamsize len = size * nmemb;
+        if (os.write(static_cast<char*>(buf), len))
+            return len;
+    }
+
+    return 0;
+}
+
+CURLcode getImageFromSnapmaker(string ipAddress)
+{
+    CURL* cameraPos;
+    cameraPos = curl_easy_init();
+
+    CURLcode result;
+
+    auto targetUrlRequest = std::format("http://{}:8080/api/request_capture_photo?index=0&x=232&y=178&z=290&feedRate=3000&photoQuality=0", ipAddress);
+    
+    std::cout << GetTimeStamp() << "\t-> cURL: Sending request to camera... ";
+    
+    curl_easy_setopt(cameraPos, CURLOPT_URL, targetUrlRequest.c_str());
+    curl_easy_setopt(cameraPos, CURLOPT_HTTPGET, 1);
+    curl_easy_setopt(cameraPos, CURLOPT_WRITEFUNCTION, NULL);
+    result = curl_easy_perform(cameraPos);
+    std::cout << std::endl;
+
+    if (result == CURLE_OK)
+    {
+        std::ofstream ofs(targetFile.string(), std::ostream::binary);
+
+        CURL* cameraImage;
+        cameraImage = curl_easy_init();
+
+        string targetUrlGetImage = std::format("http://{}:8080/api/get_camera_image?index=0", ipAddress);
+
+        std::cout << GetTimeStamp() << "\t-> cURL: Retrieving image... ";
+        curl_easy_setopt(cameraImage, CURLOPT_URL, targetUrlGetImage.c_str());
+        curl_easy_setopt(cameraImage, CURLOPT_HTTPGET, 1);
+        curl_easy_setopt(cameraImage, CURLOPT_FILE, &ofs);
+        curl_easy_setopt(cameraImage, CURLOPT_WRITEFUNCTION, &data_write);
+        result = curl_easy_perform(cameraImage);
+        std::cout << std::endl;
+
+        curl_easy_cleanup(cameraImage);
+
+        ofs.close();
+    }
+
+    curl_easy_cleanup(cameraPos);
+
+    if (result != CURLE_OK)
+    {
+        std::cout << GetTimeStamp() <<  "\tcURL: Error retrieving image" << std::endl;
+    }
+
+    return result;
+}
 
 int main()
 {
-    //CURL* curl;
-    //CURLcode result;
-    //string readBuffer;
+    targetFile = std::filesystem::current_path() / "latest.jpg";
+    auto saveFile = std::filesystem::current_path() / "latest1.jpg";
+    scCamera cam = scCreateCamera(1024, 1280, 60);
 
-    FILE* fileStream;
-    fopen_s(&fileStream, "C:\\Users\\trist\\Desktop\\example.jpg", "rb");
+    std::cout << GetTimeStamp() << "Virtual camera has started" << std::endl;
+    std::cout << GetTimeStamp() << "Press RETURN to request a new image from base position (warning: will move bed & laser!)" << std::endl;
 
     int width, height, comp;
-
-    auto image = stbi_load_from_file(fileStream, &width, &height, &comp, 0);
-    
-    scCamera cam = scCreateCamera(width, height, 1);
-
-    std::cout << "Virtual camera has started" << std::endl;
+    auto image = stbi_load(targetFile.string().c_str(), &width, &height, &comp, STBI_rgb);
 
     for (;;)
     {
         scSendFrame(cam, image);
+        
+        if (GetKeyState(VK_RETURN) & 0x8000)
+        {
+            std::cout << GetTimeStamp() << "New image requested, please wait..." << std::endl;
+
+            if (CURLE_OK == getImageFromSnapmaker(ipAddress))
+            {
+                image = stbi_load(targetFile.string().c_str(), &width, &height, &comp, STBI_rgb);
+
+                if (stbi_failure_reason() && (string("bad png sig").compare(stbi_failure_reason()) != 0))
+                    std::cout << GetTimeStamp() << "\t-> STBI: " << stbi_failure_reason() << std::endl;
+
+                std::cout << GetTimeStamp() << "Image sent to virtual camera" << std::endl;
+            }
+        }
     }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
